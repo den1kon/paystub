1fbb5e997d8584d356f139edfa9a7e93f52fee43
den1kon <dkondratiuk06@gmail.com>
2026-03-11 18:05:13 +0100

    feat(backend): add Company entity

---
+++ backend/src/Entity/Company.php
@@ -0,0 +1,94 @@
+<?php
+
+// Entity/Company.php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use DateTime;
+
+class Company
+{
+    private ?int $id = null;
+    private DateTime $createdAt;
+    private DateTime $updatedAt;
+    private string $name;
+    private ?string $alias = null;
+    private bool $isDeleted = false;
+
+    public function __construct(
+        string $name,
+        ?string $alias = null,
+    ) {
+        $this->name = $name;
+        $this->alias = $alias;
+    }
+
+    // Getters/Setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getAlias(): ?string
+    {
+        return $this->alias;
+    }
+
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    public function isDeleted(): bool
+    {
+        return $this->isDeleted;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function setAlias(?string $alias): void
+    {
+        $this->alias = $alias;
+    }
+
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
+
+    public function delete(): void
+    {
+        $this->isDeleted = true;
+    }
+
+    public function restore(): void
+    {
+        $this->isDeleted = false;
+    }
+}
